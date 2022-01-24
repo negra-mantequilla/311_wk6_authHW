9de53f26092eb1a4bddcc48fe015182d3d9c2c54
Joe Perry <japerry16633@gmail.com>
2022-01-24 12:21:45 -0600

    first commit

---
+++ utils/checkjwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const supersecret = process.env.SUPERSECRET
+
+
+function checkJwt (req,res, next){
+  let token
+  if(!req.headers.authorization) {
+    token = null
+    res.status(401).send('your not authorized')
+  } else{
+    console.log(req.headers.authorization);
+    let bearer = req.headers.authorization.split(' ')
+    console.log(bearer);
+    token = bearer[1]
+    jwt.verify(token, supersecret, (err, decoded)=>{
+     if(err){
+       res.status(401).send('your not authorized')
+     } else {
+       console.log(decoded);
+       next()
+     }
+    })
+  }
+}
+
+module.exports = {checkJwt}
