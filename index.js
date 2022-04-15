accf65a00e6710c705c265b0d02d438e2c988449
Joe Perry <japerry16633@gmail.com>
2022-04-15 11:25:12 -0500

    new edit

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-require("dotenv").config()
-const cors = require('cors')
-const {checkJwt} = require('./utils/checkjwt')
+require("dotenv").config();
+const cors = require("cors");
+const { checkJwt } = require("./utils/checkjwt");
 
-const express = require('express')
+const express = require("express");
 
-
-
-const userRoutes = require('./routes/users')
-const clientsRoutes = require ('./routes/clients')
+const userRoutes = require("./routes/users");
+const clientsRoutes = require("./routes/clients");
 
 // const router = require('./routes/routes')
 
+const app = express();
 
-const app = express()
+const connection = require("./SQL/connection");
 
-const connection = require('./SQL/connection')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.options("*", cors());
 
-app.use(cors())
-app.options('*', cors())
-app.use(express.json())
-app.use(userRoutes)
-app.use(clientsRoutes)
+app.use(userRoutes);
+app.use(clientsRoutes);
 
+const port = process.env.PORT || 8005;
 
-const port = process.env.PORT||8005
-
-app.get('/', (req, res)=>{
-  res.json('Hello World')
+app.get("/", (req, res) => {
+  res.json("Hello World");
   connection.connect(function () {
     console.log("connected");
-})
-})
+  });
+});
 
-
-
-
-
-app.listen(port, function(){
-  console.log('listening on PORT ', port);
-})
+app.listen(port, function () {
+  console.log("listening on PORT ", port);
+});
