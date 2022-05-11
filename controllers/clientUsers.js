a69e16fa5df0dcd8ca50389158e680b5bc44dc3c
Joe Perry <japerry16633@gmail.com>
2022-05-11 13:22:31 -0500

    another push

--- controllers/clientUsers.js
+++ controllers/clientUsers.js
@@ -1,12 +1,19 @@
 const connection = require("../SQL/connection");
 
-function getClientUserRelation(req, res) {
+function getClientUserRelationById(req, res) {
   const sql = `SELECT u.user_id, u.user_name, c.full_name, c.bio FROM users AS u
-JOIN clients as c WHERE c.user_id = u.user_id
-AND u.user_id = ?;`;
+    JOIN clients as c WHERE c.user_id = u.user_id
+    AND u.user_id = ?;`;
   connection.query(sql, req.user_id, (err, results) => {
     res.json(results);
   });
 }
+function getClientUserRelation(req, res) {
+  const sql = `SELECT clients_id, full_name, bio, users.user_id, users.user_name 
+    FROM clients LEFT JOIN users ON users.user_id = clients.user_id`;
+  connection.query(sql, (err, results) => {
+    res.json(results);
+  });
+}
 
-module.exports = { getClientUserRelation };
+module.exports = { getClientUserRelationById, getClientUserRelation };
--- routes/clientUsers.js
+++ routes/clientUsers.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {checkJwt} = require('../utils/checkjwt')
 const clientUsersController = require('../controllers/clientUsers')
 
-router.get("/client-user-relation",checkJwt,clientUsersController.getClientUserRelation)
+router.get("/client-user-relation-by-user",checkJwt,clientUsersController.getClientUserRelationById)
+router.get("/client-user-relation",clientUsersController.getClientUserRelation)
 
 module.exports = router
